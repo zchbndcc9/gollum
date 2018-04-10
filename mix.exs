ad7c84a3223a8c5b270848c29b7bb68f46cea94f
Mike Benner <refriedchicken@gmail.com>
2018-04-10 09:47:16 -0700

    update version number

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Gollum.Mixfile do
   def project do
     [
       app: :gollum,
-      version: "0.2.2",
+      version: "0.2.3",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env),
       start_permanent: Mix.env == :prod,
